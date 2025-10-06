7feb4fb0c6284d3e7c5f297bd0826e8803452197
Lisboa-dev <matheusllisboas@gmail.com>
2025-10-05 21:42:34 -0300

    atualização: package e prisma/schema, correção: app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import userRouter from "./User/router";
 import  loteRouter  from "./Lote/router";
 import  produtoRouter  from "./Produto/router";
 import  generoRouter  from "./Genero/router";
-import { authenticateToken } from "./middlewares/JWT/middleware.js";
+import { authenticateToken } from "./middlewares/JWT/authMiddleware.js";
 
 
 const app: Express = express();
@@ -14,13 +14,13 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.use('/')
-app.use('/users', authenticateToken, userRouter);
+//app.use('/')
+app.use('/users', userRouter);
 app.use('/lotes', authenticateToken, loteRouter);
 app.use('/produtos', authenticateToken, produtoRouter);
 app.use('/generos', authenticateToken, generoRouter);
 
 
 app.listen(port, () => {
-    console.log(`A API subiu na porta ${port}`)
+    console.log(`A API subiu na porta ${port}, o endereço é http://localhost:${port}`)
 });
