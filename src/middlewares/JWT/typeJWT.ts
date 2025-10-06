b09d8e8eb614ff732f546333503cb209803a5c0b
Lisboa-dev <matheusllisboas@gmail.com>
2025-10-05 21:48:40 -0300

    atualização do middleware de  autenticação

--- src/middlewares/JWT/typeJWT.ts
+++ src/middlewares/JWT/typeJWT.ts
@@ -3,9 +3,11 @@ import { Request } from 'express';
 export interface UserPayload {
   id: string;
   email: string;
-  // Adicione outras propriedades do usuário que deseja incluir no token
 }
 
 export interface AuthRequest extends Request {
   user?: UserPayload;
 }
+
+
+
